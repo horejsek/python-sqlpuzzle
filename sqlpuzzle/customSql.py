39e0d9b2a3ef24cf94a373f66547f57121895601
Michal Horejsek <horejsekmichal@gmail.com>
2011-07-05 14:06:02 +0200

    in customsql add fake method for comparison

--- sqlpuzzle/customSql.py
+++ sqlpuzzle/customSql.py
@@ -8,8 +8,14 @@
 
 class CustomSql(object):
     def __init__(self, string=''):
+        """Initialization of CustomSql"""
         self.__string = string
     
     def __str__(self):
+        """Print custom SQL"""
         return self.__string
+    
+    def __getattr__(self, attr):
+        """Fake method for camparison"""
+        return None
 
