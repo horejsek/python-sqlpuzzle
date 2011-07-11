fa6dd0e99d2cd27e8afae3651992e41d7ad28834
Michal Horejsek <horejsekmichal@gmail.com>
2011-07-11 18:34:33 +0200

    allTests for tools

--- tests/allTests.py
+++ tests/allTests.py
@@ -23,29 +23,31 @@ import libs.sqlValueTest
 import sqlPuzzleTest
 
 
-if __name__ == '__main__':
-    testModules = (
-        queries.deleteTest,
-        queries.insertTest,
-        queries.selectTest,
-        queries.unionTest,
-        queries.updateTest,
-        
-        features.columnsTest,
-        features.groupByTest,
-        features.havingTest,
-        features.limitTest,
-        features.orderByTest,
-        features.tablesTest,
-        features.valuesTest,
-        features.whereTest,
-        
-        libs.argsParserTest,
-        libs.sqlValueTest,
-        
-        sqlPuzzleTest,
-    )
+testModules = (
+    queries.deleteTest,
+    queries.insertTest,
+    queries.selectTest,
+    queries.unionTest,
+    queries.updateTest,
     
+    features.columnsTest,
+    features.groupByTest,
+    features.havingTest,
+    features.limitTest,
+    features.orderByTest,
+    features.tablesTest,
+    features.valuesTest,
+    features.whereTest,
+    
+    libs.argsParserTest,
+    libs.sqlValueTest,
+    
+    sqlPuzzleTest,
+)
+
+
+# autorun
+if __name__ == '__main__':
     testCases = []
     for testModule in testModules:
         testCases += testModule.testCases
@@ -54,4 +56,22 @@ if __name__ == '__main__':
     for testCase in testCases:
         suite.addTests(unittest.TestLoader().loadTestsFromTestCase(testCase))
     unittest.TextTestRunner(verbosity=0).run(suite)
+
+# for tools
+else:
+    class UniqName:
+        def __init__(self):
+            self.__inc = 0
+        def __str__(self):
+            self.__inc += 1
+            return "uniqName_%d" % self.__inc
+    uniqName = UniqName()
+    
+    # all tests object add to the global scope with unique name
+    globals_ = globals()
+    for testModule in testModules:
+        for testCase in testModule.testCases:
+            globals()[str(uniqName)] = testCase
+
+
 
