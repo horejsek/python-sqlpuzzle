5c158e6986adaa7d7b0d1577b7b6f388a25d669c
Michal Horejsek <horejsekmichal@gmail.com>
2013-06-08 09:21:41 +0200

    Change licence to PSF

--- setup.py
+++ setup.py
@@ -4,13 +4,6 @@
 # Michal Horejsek <horejsekmichal@gmail.com>
 # https://github.com/horejsek/python-sqlpuzzle
 #
-# This application is released under the GNU General Public License
-# v3 (or, at your option, any later version). You can find the full
-# text of the license under http://www.gnu.org/licenses/gpl.txt.
-# By using, editing and/or distributing this software you agree to
-# the terms and conditions of this license.
-# Thank you for using free software!
-#
 
 from distutils.core import setup
 
@@ -29,11 +22,11 @@ setup(
     description='Python library for writing SQL queries.',
     author='Michal Horejsek',
     author_email='horejsekmichal@gmail.com',
-    license='GNU General Public License (GPL)',
+    license='PSF',
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'License :: OSI Approved :: Python Software Foundation License',
         'Operating System :: OS Independent',
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
