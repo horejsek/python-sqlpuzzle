868d24e5e135a1d14b554a3678d448a790b51c72
Michal Horejsek <horejsekmichal@gmail.com>
2013-06-13 20:52:41 +0200

    Run tests only once

--- tests/alltests.py
+++ tests/alltests.py
@@ -4,5 +4,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__) + '/../'))
 
-from nose import run
-run(argv=[sys.argv[0], '--include', '.*test(\.py)?'])
+if __name__ == '__main__':
+    from nose import run
+    run(argv=[sys.argv[0], '--all-modules'])
